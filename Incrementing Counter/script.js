2ca30cd66a3783f736d86c37e753f6fc4b1e4b91
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-04-11 14:17:56 -0400

    15/50 done

---
+++ Incrementing Counter/script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach((counter) => {
+  counter.innerText = '0'
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target') // '+' before counter turns target into a number
+    const c = +counter.innerText
+
+    const increment = target / 200
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+    } else {
+      counter.innerText = target
+    }
+  }
+
+  updateCounter()
+})
